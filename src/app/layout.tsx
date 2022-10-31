ca7885419540f890ec3d3b907bc83ddc738d4716
Abhinay Parvathaneni <pabhinay43@gmail.com>
2022-10-30 23:12:43 -0400

    app folder support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Link from 'next/link'
-import {ChatFill} from '@styled-icons/bootstrap/ChatFill'
+import 'styles/globals.scss'
+import 'styles/buttons.scss'
+import 'styles/font.scss'
+import 'styles/animations.scss'
 
 interface HeaderProps {
   href: string,
@@ -12,13 +15,13 @@ const HeaderLink: React.FC = (props: HeaderProps) => {
   delete _no_child_props.children
 
   return (
-    <Link {..._no_child_props}>
+    <Link className='items-center flex' {..._no_child_props}>
       <span className="cursor-pointer font-light px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-90">{props.children}</span>
     </Link>
   )
 }
 
-const Layout: React.FC = ({ children, noSpacing=false }: { children: React.FC, noSpacing: boolean }) => {
+const Layout: React.FC = ({ children }: { children: React.FC }) => {
   return (
     <>
       <div className="p-4 border-b">
@@ -29,13 +32,10 @@ const Layout: React.FC = ({ children, noSpacing=false }: { children: React.FC, n
           </nav>
           <div className="flex space-x-6 mr-3">
             <button className="btn btn-secondary">Feedback</button>
-            <button className="btn btn-primary shadow-shinePrimary">
-              <ChatFill size="14" className="mr-2" /> Chat
-            </button>
           </div>
         </div>
       </div>
-      <div className={`container mx-auto ${noSpacing ? '' : 'p-4'}`}>
+      <div className={`container mx-auto p-4`}>
         {children}
       </div>
     </>
